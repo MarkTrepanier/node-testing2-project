452b70ff574c326399f17f92b1725f4b63ec124a
MarkTrepanier <marktrepanier.cajigas@gmail.com>
2021-11-11 18:08:51 -0500

    [GET]

--- api/server.test.js
+++ api/server.test.js
@@ -1,6 +1,7 @@
 const server = require("./server");
 const request = require("supertest");
 const db = require("../data/db-config");
+const { items } = require("../data/seeds/01-initial-items");
 
 beforeAll(async () => {
   await db.migrate.rollback();
@@ -15,8 +16,12 @@ afterAll(async () => {
 
 describe("[GET] api/items", () => {
   test("responds with items", async () => {
-    const res = await request(server).get("api/items");
+    const res = await request(server).get("/api/items");
     expect(res.status).toBe(200);
     expect(res.body).toHaveLength(7);
+  });
+  test("return follows shape", async () => {
+    const res = await request(server).get("/api/items");
+    expect(res.body).toMatchObject(items);
   });
 });
